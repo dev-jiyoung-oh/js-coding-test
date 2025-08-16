c49a121d2a7dcfaea3ec5887500b4b2cbd9d37aa
jiyoung <aa991102@naver.com>
2025-08-17 03:13:23 +0900

    250817-프로그래머스-133499-옹알이 (2)

---
+++ 250817_programmers_133499.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/133499
+// 옹알이 (2)
+
+function solution(babbling) {
+  const regexp1 = /(aya|ye|woo|ma)\1+/; // *연속 반복 검출
+  const regexp2 = /^(aya|ye|woo|ma)+$/; // 문자열이 전부 옹알이 단어로만 구성되어 있는지 확인
+
+  return babbling.reduce((ans, word) => (
+    !regexp1.test(word) && regexp2.test(word) ? ++ans : ans
+  ), 0);
+}
