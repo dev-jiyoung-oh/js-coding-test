7706f027ea8cb5b4c28319be95bf14fa4d48c824
jiyoung <aa991102@naver.com>
2025-09-11 14:01:27 +0900

    250911-프로그래머스-12985-예상 대진표

---
+++ 250911_programmers_12985.js
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12985
+// 예상 대진표
+
+function solution(n, a, b) {
+    let answer = 0;
+    while(a !== b) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        answer++;
+    }
+
+    return answer;
+}
